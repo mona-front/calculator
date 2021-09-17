eac5f5c721e4074320fb63e61956b0bfd5dbdb86
mona behkerdar <monabehkerdar@gmail.com>
2021-09-17 21:22:32 +0430

    first commit

---
+++ min.js
@@ -0,0 +1,28 @@
+const doller=document.querySelector("#inputText")
+const personnumber=document.querySelector("#personNumber")
+const amountpeople=document.querySelector(".amountPeople")
+const totalpeople=document.querySelector(".totalPeople")
+const persond=document.querySelectorAll(".number")
+const reset=document.querySelector(".reset")
+const number2=document.querySelector("#number")
+const custom=document.querySelector(".custom")
+const input=document.querySelectorAll(["input"])
+
+
+// create event and function
+    for(let i =0;i<persond.length;i++){
+        persond[i].addEventListener("click",()=>{
+            let amount= (doller.value * persond[i].value)/personnumber.value;
+            amountpeople.innerText=amount+ "$"
+            let total=(doller.value * persond[i].value)
+            totalpeople.innerText=total+ "$"
+
+        })
+    }
+
+//  
+reset.addEventListener("click",()=>{
+    totalpeople.innerText="0.0";
+    amountpeople.innerText="0.0"
+})
+
