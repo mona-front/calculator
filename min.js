ea58465718f3386e1ed73a3af5c318fc0f133311
mona behkerdar <monabehkerdar@gmail.com>
2021-09-18 15:33:48 +0430

    update files

--- min.js
+++ min.js
@@ -26,3 +26,9 @@ reset.addEventListener("click",()=>{
     amountpeople.innerText="0.0"
 })
 
+custom.addEventListener("onchange", ()=>{
+    let amount = (dollar.value  / 100 ) * custom.value /personnumber.value;/* there is a problem in run this function */
+    amountpeople.innerText=amount+ "$"
+    let total=doller.value * custom.value.value
+      totalpeople.innerText=total+ "$"
+})
