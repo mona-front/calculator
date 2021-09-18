53a996c48e247dba0f3024cfff8a37a109f2857e
mona behkerdar <monabehkerdar@gmail.com>
2021-09-18 17:13:35 +0430

    delet some files and update a few files

--- min.js
+++ min.js
@@ -2,29 +2,31 @@ const doller=document.querySelector("#inputText")
 const personnumber=document.querySelector("#personNumber")
 const amountpeople=document.querySelector(".amountPeople")
 const totalpeople=document.querySelector(".totalPeople")
-const persond=document.querySelectorAll(".number")
+const percent=document.querySelectorAll(".number")
 const reset=document.querySelector(".reset")
 const number2=document.querySelector("#number")
 const custom=document.querySelector(".custom")
-const input=document.querySelectorAll(["input"])
+// const input=document.querySelectorAll(["input"])
 
 
 // create event and function
-    for(let i =0;i<persond.length;i++){
-        persond[i].addEventListener("click",()=>{
-            let amount= (doller.value * persond[i].value)/personnumber.value;
+    for(let i =0;i<percent.length;i++){
+        percent[i].addEventListener("click",()=>{
+            validation()/* validate form person*/ 
+            let amount= (doller.value * percent[i].value)/personnumber.value;
             amountpeople.innerText=amount+ "$"
-            let total=(doller.value * persond[i].value)
+            let total=(doller.value * percent[i].value)
             totalpeople.innerText=total+ "$"
 
         })
     }
 
-//  
+//  empty form
 reset.addEventListener("click",()=>{
     totalpeople.innerText="0.0";
     amountpeople.innerText="0.0"
 })
+// calc custom persond
 
 custom.addEventListener("onchange", ()=>{
     let amount = (dollar.value  / 100 ) * custom.value /personnumber.value;/* there is a problem in run this function */
@@ -32,3 +34,10 @@ custom.addEventListener("onchange", ()=>{
     let total=doller.value * custom.value.value
       totalpeople.innerText=total+ "$"
 })
+
+// validation
+const validation =()=>{
+    if(personnumber.value==="" &&  doller.value===""){
+        alert("لطفا کادر ها را پر کنید ")
+     }
+}
